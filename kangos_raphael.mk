f87b0f0faedde5c0bd29dc7195d34941d3a6adee
Sherif Rahim <sherifrahim2001@gmail.com>
2021-03-20 20:05:27 +0500

    KangOS:bringup

--- kangos_raphael.mk
+++ kangos_raphael.mk
@@ -1,42 +1,25 @@
-# Copyright (C) 2021 kangos
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Copyright (C) 2020-2021 The LineageOS Project
+# Copyright (C) 2020-2021 The Evolution X Project
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# SPDX-License-Identifier: Apache-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-# Inherit from those products. Most specific first.
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common kangos stuff.
+# Inherit some common KangOS stuff.
+KANGOS_BUILDTYPE := OFFICIAL
+TARGET_USES_BLUR := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+USE_GAPPS := false
+
 $(call inherit-product, vendor/kangos/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := kangos_raphael
-PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := MI 9T Pro
+PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="raphael"
+PRODUCT_NAME := kangos_raphael
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# GApps
-ifeq ($(USE_GAPPS),true)
-TARGET_INCLUDE_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ARCORE := true
-endif
-
-# Official
-FLUID_BUILD_TYPE := OFFICIAL
